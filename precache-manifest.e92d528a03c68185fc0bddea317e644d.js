f0d79d795f1eb10c058ea379349fa23b878c23b6
unknown <jonasleonas94@gmail.com>
2020-08-02 21:15:06 -0500

    Updates

---
+++ precache-manifest.e92d528a03c68185fc0bddea317e644d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4abff57336f12e63714f2476f0f3a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4deb76b9f17e6269753",
+    "url": "/static/css/main.c4455299.chunk.css"
+  },
+  {
+    "revision": "ff4f725e254d56de94e1",
+    "url": "/static/js/2.e5a1691b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5a1691b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4deb76b9f17e6269753",
+    "url": "/static/js/main.e557ca55.chunk.js"
+  },
+  {
+    "revision": "2ef73804eaba18394387",
+    "url": "/static/js/runtime-main.01de51f8.js"
+  }
+]);
