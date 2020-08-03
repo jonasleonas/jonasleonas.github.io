c93bccaefd81584e5e7f4d8086c2f1f3ebc3640a
unknown <jonasleonas94@gmail.com>
2020-08-02 21:10:21 -0500

    First commit

---
+++ cleaning.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  1 19:41:18 2020
+
+@author: jonas
+"""
+
+
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+import Chart from "./components/Chart";
+
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Chart />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
